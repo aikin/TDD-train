43233bbfda80f8247669bd451d6d63ba794431b0
aikin <luajin312@gmail.com>
2014-11-17 23:19:09 +0800

    aikin: change test to make doors count be input

--- 100-doors/test/door.spec.js
+++ 100-doors/test/door.spec.js
@@ -1,6 +1,6 @@
 var door = require('../src/door');
 
-describe('100 doors', function() {
+describe('use 100 doors when input doors be empty', function() {
 
     var doors;
     beforeEach(function() {
@@ -26,4 +26,23 @@ describe('100 doors', function() {
         expect(doors.opened.toString()).toBe([1, 4, 9, 16, 25, 36, 49, 64, 81, 100].toString());
     });
 
+});
+
+describe('fetch right result when input doors count', function() {
+
+    it('should tip when parameter not be number', function() {
+
+        expect(door('not be number')).toEqual('sorry, parameter must be number');
+    });
+
+    it('should return right opened doors for parameter', function() {
+
+        expect(door(10).opened.toString()).toBe([1, 4, 9].toString());
+        expect(door(15).opened.toString()).toBe([1, 4, 9].toString());
+        expect(door(16).opened.toString()).toBe([1, 4, 9, 16].toString());
+        expect(door(24).opened.toString()).toBe([1, 4, 9, 16].toString());
+        expect(door(40).opened.toString()).toBe([1, 4, 9, 16, 25, 36].toString());
+        expect(door(120).opened.toString()).toBe([1, 4, 9, 16, 25, 36, 49, 64, 81, 100].toString());
+        expect(door(121).opened.toString()).toBe([1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121].toString());
+    });
 });
