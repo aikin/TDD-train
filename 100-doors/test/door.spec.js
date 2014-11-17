e427637391cd970e3bfdc0db22abc51d93cc18c3
aikin <luajin312@gmail.com>
2014-11-17 23:11:03 +0800

    aikin: add new test unit on 100-doors

--- 100-doors/test/door.spec.js
+++ 100-doors/test/door.spec.js
@@ -17,4 +17,13 @@ describe('100 doors', function() {
         expect(doors.closed instanceof Array).toBe(true);
     });
 
+    it('should opened doors count be 10, closed doors count be 90', function() {
+        expect(doors.opened.length).toBe(10);
+        expect(doors.closed.length).toBe(90);
+    });
+
+    it('should opened doors 1, 4, 9, 16, 25, 36, 49, 64, 81, 100', function() {
+        expect(doors.opened.toString()).toBe([1, 4, 9, 16, 25, 36, 49, 64, 81, 100].toString());
+    });
+
 });
