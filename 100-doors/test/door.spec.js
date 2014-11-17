6ac1116b259173af1c1c616d26ca3a2ad7508285
aikin <luajin312@gmail.com>
2014-11-17 23:40:53 +0800

    aikin: change door.spec.js

--- 100-doors/test/door.spec.js
+++ 100-doors/test/door.spec.js
@@ -1,10 +1,11 @@
-var door = require('../src/door');
+var door   = require('../src/door');
+var utils  = require('../src/utils');
 
 describe('use 100 doors when input doors be empty', function() {
 
     var doors;
     beforeEach(function() {
-         doors = door();
+         doors = door(utils);
     });
 
     afterEach(function() {
@@ -32,17 +33,17 @@ describe('fetch right result when input doors count', function() {
 
     it('should tip when parameter not be number', function() {
 
-        expect(door('not be number')).toEqual('sorry, parameter must be number');
+        expect(door('not be number', utils)).toEqual('sorry, parameter must be number');
     });
 
     it('should return right opened doors for parameter', function() {
 
-        expect(door(10).opened.toString()).toBe([1, 4, 9].toString());
-        expect(door(15).opened.toString()).toBe([1, 4, 9].toString());
-        expect(door(16).opened.toString()).toBe([1, 4, 9, 16].toString());
-        expect(door(24).opened.toString()).toBe([1, 4, 9, 16].toString());
-        expect(door(40).opened.toString()).toBe([1, 4, 9, 16, 25, 36].toString());
-        expect(door(120).opened.toString()).toBe([1, 4, 9, 16, 25, 36, 49, 64, 81, 100].toString());
-        expect(door(121).opened.toString()).toBe([1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121].toString());
+        expect(door(10, utils).opened.toString()).toBe([1, 4, 9].toString());
+        expect(door(15, utils).opened.toString()).toBe([1, 4, 9].toString());
+        expect(door(16, utils).opened.toString()).toBe([1, 4, 9, 16].toString());
+        expect(door(24, utils).opened.toString()).toBe([1, 4, 9, 16].toString());
+        expect(door(40, utils).opened.toString()).toBe([1, 4, 9, 16, 25, 36].toString());
+        expect(door(120, utils).opened.toString()).toBe([1, 4, 9, 16, 25, 36, 49, 64, 81, 100].toString());
+        expect(door(121, utils).opened.toString()).toBe([1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121].toString());
     });
 });
