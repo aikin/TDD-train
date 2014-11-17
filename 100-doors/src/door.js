e5c59ca1f206e57a25d0c8b46b038e008f1c6cc3
aikin <luajin312@gmail.com>
2014-11-17 23:12:24 +0800

    aikin: pass test

--- 100-doors/src/door.js
+++ 100-doors/src/door.js
@@ -1,6 +1,27 @@
 function door() {
 
-    return { opened: [], closed: [] };
+    var doors  = [];
+    var result = { opened: [], closed: [] };
+
+    for (var i = 0; i < 100; i++) {
+        doors[i + 1] = false;
+    }
+
+    for (var m = 1; m <= 100; m++) {
+        for (var n = m - 1; n < 100; n += m) {
+            doors[n] = !doors[n];
+        }
+    }
+
+    for (var j = 1; j <= 100; j++) {
+        if (doors[j - 1]) {
+            result.opened.push(j);
+        } else {
+            result.closed.push(j);
+        }
+    }
+
+    return result;
 }
 
 module.exports = door;
