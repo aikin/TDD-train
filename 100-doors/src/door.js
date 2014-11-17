175f8fea7c75aa4d52e125355c9bf68eb14bebf1
aikin <luajin312@gmail.com>
2014-11-17 23:48:03 +0800

    aikin: refactor

--- 100-doors/src/door.js
+++ 100-doors/src/door.js
@@ -1,27 +1,19 @@
-function door(count) {
+function door(count, utils) {
 
-    count = count || 100;
+    if (typeof count === 'object') {
+        utils = count;
+        count = 100;
+    }
 
     if (typeof count !== 'number') return 'sorry, parameter must be number';
 
-    var doors  = [];
     var result = { opened: [], closed: [] };
 
-    for (var i = 0; i < count; i++) {
-        doors[i + 1] = false;
-    }
-
-    for (var m = 1; m <= count; m++) {
-        for (var n = m - 1; n < count; n += m) {
-            doors[n] = !doors[n];
-        }
-    }
-
-    for (var j = 1; j <= count; j++) {
-        if (doors[j - 1]) {
-            result.opened.push(j);
+    for (var i = 1; i <= count; i++) {
+        if (utils.countFactor(i) % 2 !== 0) {
+            result.opened.push(i)
         } else {
-            result.closed.push(j);
+            result.closed.push(i);
         }
     }
 
