b3d809ff21e674427206f3fca051078b831b90ff
aikin <luajin312@gmail.com>
2014-11-19 21:50:12 +0800

    aikin - add test

--- anagrams/test/anagrams_spec.js
+++ anagrams/test/anagrams_spec.js
@@ -4,9 +4,30 @@ var anagrams = require('../src/anagrams');
 describe('test anagrams', function() {
 
     it('should input correct string', function() {
+
         anagrams(1234).should.to.equal('input must be string.');
         anagrams(true).should.to.equal('input must be string.');
         anagrams({spec: 'spec'}).should.to.equal('input must be string.');
-        anagrams('').should.to.equal('input string must not be empty');
+        anagrams('').should.to.equal('input string must not be empty.');
+    });
+
+    it('should return all potential anagrams', function() {
+
+        anagrams('a').should.to.eql(['a']);
+        anagrams('ai').should.to.have.length(2);
+        anagrams('ai').should.to.eql(['ai', 'ia']);
+        anagrams('aik').should.to.have.length(6);
+        anagrams('aik').should.to.eql(['aik', 'aki', 'iak', 'ika', 'kia', 'kai']);
+        anagrams('ai,').should.to.have.length(6);
+        anagrams('ai,').should.to.eql(['ai,', 'a,i', 'ia,', 'i,a', ',ia', ',ai']);
+        var expect = [
+            'biro', 'bior', 'brio', 'broi', 'bori', 'boir',
+            'ibro', 'ibor', 'irbo', 'irob', 'iorb', 'iobr',
+            'ribo', 'riob', 'rbio', 'rboi', 'robi', 'roib',
+            'oirb', 'oibr', 'orib', 'orbi', 'obri', 'obir'
+        ];
+        anagrams('biro').should.to.have.length(expect.length);
+        anagrams('biro').should.to.eql(expect);
+
     });
 });
