f431176807f7a0fb95929d7ca9f3ec03864591d4
aikin <luajin312@gmail.com>
2014-12-14 21:36:07 +0800

    aikin - refactor

--- bowling-game/src/game.js
+++ bowling-game/src/game.js
@@ -21,15 +21,15 @@ Game.prototype.score = function() {
 
     for (var frame = 0; frame < 10; frame++) {
         if (this._isStrike(frameIndex)) {
-            score += 10 + this._rolls[frameIndex + 1] + this._rolls[frameIndex + 2];
+            score += 10 + this._strikeBonus(frameIndex);
             frameIndex++;
 
         } else if (this._isSpare(frameIndex)) {
-            score += 10 + this._rolls[frameIndex + 2];
+            score += 10 + this._spareBonus(frameIndex);
             frameIndex += 2;
 
         } else {
-            score += this._rolls[frameIndex] + this._rolls[frameIndex + 1];
+            score += this._sumOfBallsInFrame(frameIndex);
             frameIndex += 2;
         }
     }
@@ -44,5 +44,17 @@ Game.prototype._isStrike = function(frameIndex) {
     return this._rolls[frameIndex] === 10;
 };
 
+
+Game.prototype._sumOfBallsInFrame = function(frameIndex) {
+    return this._rolls[frameIndex] + this._rolls[frameIndex + 1];
+};
+
+Game.prototype._spareBonus = function(frameIndex) {
+    return this._rolls[frameIndex + 2];
+};
+
+Game.prototype._strikeBonus = function(frameIndex) {
+    return this._rolls[frameIndex + 1] + this._rolls[frameIndex + 2];
+};
 
 module.exports = Game;
